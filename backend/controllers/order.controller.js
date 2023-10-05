a1b824ca85bc7acd60d3134f4c85b1094ae522ea
HanawuZ <b6302245@gmail.com>
2023-10-05 12:12:38 +0700

    init other controller

---
+++ backend/controllers/order.controller.js
@@ -0,0 +1,17 @@
+async function insertOrder(req,res,next) {
+
+}
+
+async function updateOrder(req,res,next) {
+
+}
+
+async function deleteOrder(req,res,next) {
+
+}
+
+async function getOrder(req,res,next) {
+
+}
+
+module.exports = {insertOrder,updateOrder,deleteOrder, getOrder}
