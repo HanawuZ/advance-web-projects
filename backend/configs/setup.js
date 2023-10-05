70a5127ba248dabc77ae484f72eb89d781930313
HanawuZ <b6302245@gmail.com>
2023-10-05 12:07:54 +0700

    add food controller

--- backend/configs/setup.js
+++ backend/configs/setup.js
@@ -1,9 +1,26 @@
 // getting-started.js
 const mongoose = require('mongoose');
 
-await mongoose.connect('mongodb://127.0.0.1:27017/foods').catch((err) => {
-    console.log(err);
-});
+function connectToDatabase(req,res,next) {
+
+    const url = 'mongodb://127.0.0.1:27017/foods'
+    const configs = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        autoIndex: true,
+    }
+
+    mongoose.connect(url,configs)
+    .then(() => {
+        console.log('Connected to database...')
+        next()
+    }).catch(err => {
+        console.log(err)
+        res.status(501).send('Cannot connect to MongoDB');
+    })
+}
+
+module.exports = connectToDatabase
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 
