cc25258ed6fe4ab2cc5651f0e2f87432049e00f0
B6327552 <Aceportgasonepiece@gmail.com>
2023-10-05 11:39:05 +0700

    createFrontend

---
+++ frontend/src/app/pages/user/listmenu/listmenu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listmenu',
+  templateUrl: './listmenu.component.html',
+  styleUrls: ['./listmenu.component.css']
+})
+export class ListmenuComponent {
+
+}
