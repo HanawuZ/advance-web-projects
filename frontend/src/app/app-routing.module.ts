8001fe40ac6d661d3f7a3b2c0fda040c4de4659c
sudarat9 <sudarat76692@gmail.com>
2023-10-11 18:02:20 +0700

    updeate

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,12 +4,18 @@ import { ListmenuComponent } from './pages/user/listmenu/listmenu.component';
 import { CheckbillComponent } from './pages/user/checkbill/checkbill.component';
 import { HomeComponent } from './pages/user/home/home.component';
 import { LoginComponent } from './pages/admin/login/login.component';
+import { MenuComponent } from './pages/admin/menu/menu.component';
+import { AddmenuComponent } from './pages/admin/addmenu/addmenu.component';
+import { StatustableComponent } from './pages/admin/statustable/statustable.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'listmenu', component: ListmenuComponent },
   { path: 'checkbill', component: CheckbillComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'addmenu', component: AddmenuComponent },
+  { path: 'statustable', component: StatustableComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
 
