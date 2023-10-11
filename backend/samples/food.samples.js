26605e0ed91f53332d2170734476608e7cf9c447
HanawuZ <b6302245@gmail.com>
2023-10-11 15:49:45 +0700

    add http get

--- backend/samples/food.samples.js
+++ backend/samples/food.samples.js
@@ -3,55 +3,38 @@ const Food = require('../models/food')
 const Burger = new Food({
     name: 'Burger',
     price: 150,
-    picture: "google.com/image.png"
+    picture: "backend//img//menu//menu-item-1.png"
 })
 
 const Sandwich = new Food({
     name: 'Sandwich',
     price: 100,
-    picture: "google.com/image1.png"
+    picture: "backend//img//menu//menu-item-2.png"
 })
 
 const TeaMilk = new Food({
     name: 'Tea Milk',
     price: 50,
-    picture: "google.com/image2.png"
+    picture: "backend//img//menu//menu-item-3.png"
 })
 
 const Coffee = new Food({
     name: 'Coffee',
     price: 60,
-    picture: "google.com/image3.png"
+    picture: "backend//img//menu//menu-item-4.png"
 })
 
 const Cocoa = new Food({
     name: 'Cocoa',
     price: 70,
-    picture: "google.com/image4.png"
+    picture: "backend//img/menu//menu-item-5.png"
 })
 
 const Espresso = new Food({
     name: 'Espresso',
     price: 80,
-    picture: "google.com/image5.png"
+    picture: "backend//img//menu//menu-item-6.png"
 
 })
 
-const Mocca = new Food({
-    name: 'Mocca',
-    price: 69,
-    picture: "google.com/image6.png"
-})
-
-const Latte = new Food({
-    name: 'Latte',
-    price: 75,
-    picture: "google.com/image7.png"
-})
-
-const Bingsoo = new Food({
-    name: 'Bingsoo',
-    price: 120,
-    picture: "google.com/image8.png"
-})
-module.exports = { Burger , Sandwich, TeaMilk, Coffee, Cocoa, Espresso, Mocca, Latte, Bingsoo }
+module.exports = { Burger , Sandwich, TeaMilk, Coffee, Cocoa, Espresso }
