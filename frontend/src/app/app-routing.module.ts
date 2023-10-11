b88466e5345411bd7c347099a7391703724c9d77
B6327552 <Aceportgasonepiece@gmail.com>
2023-10-11 17:00:57 +0700

    link path

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListmenuComponent } from './pages/user/listmenu/listmenu.component';
+import { CheckbillComponent } from './pages/user/checkbill/checkbill.component';
+import { HomeComponent } from './pages/user/home/home.component';
+import { LoginComponent } from './pages/admin/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'listmenu', component: ListmenuComponent },
+  { path: 'checkbill', component: CheckbillComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
