0acc645775a155ba95d0f72fbcd21f1f4763450f
B6327552 <Aceportgasonepiece@gmail.com>
2023-10-12 14:56:51 +0700

    update navbar

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
-
+  isAdmin: boolean;
+  
+  constructor() {
+    const token = localStorage.getItem('token');
+    this.isAdmin = token !== null;
+  }
 }
