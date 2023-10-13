30355ebc5b079bb0706fc4dbf926b94e96c47ade
B6327552 <Aceportgasonepiece@gmail.com>
2023-10-13 12:01:29 +0700

    user checkbill

--- frontend/src/app/pages/user/checkbill/checkbill.component.ts
+++ frontend/src/app/pages/user/checkbill/checkbill.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-checkbill',
   templateUrl: './checkbill.component.html',
-  styleUrls: ['./checkbill.component.css']
+  styleUrls: ['./checkbill.component.css'],
 })
 export class CheckbillComponent {
+  order: any = {};
+  orderedFood: any[] = [];
+  constructor() {}
 
+  getOrdered() {
+    fetch('http://localhost:3000/order')
+      .then((response) => response.json())
+      .then((data) => {
+        this.order = data[0];
+        console.log(this.order);
+      })
+      .catch((error) => console.error(error));
+  }
+  
+  getOrderedFood() {
+    fetch('http://localhost:3000/ordered_food')
+    .then((response) => response.json())
+    .then((data) => {
+      this.orderedFood = data;
+      console.log(this.orderedFood);
+    })
+    .catch((error) => console.error(error));
+  }
+
+  ngOnInit(): void {
+    this.getOrdered();
+    this.getOrderedFood();
+  }
 }
