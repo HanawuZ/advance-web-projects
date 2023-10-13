23e64e1cb3924fabc68cfc3b64712a7cee02c774
HanawuZ <b6302245@gmail.com>
2023-10-13 17:35:28 +0700

    improve insertOrder

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -20,30 +20,70 @@ async function listOrder(req, res, next) {
 
 async function insertOrder(req, res, next) {
     const order_foods = req.body.order_foods
- 
+    const table_id = req.body.table_id
+    
     order_foods.forEach(async (order_food) => {
         await Ordered_food.findOneAndDelete( {_id : order_food._id} )
     });
+    
+    // Find existing order with this table_id
+    const existedOrder = await Order.findOne({ table_id: table_id })
 
-    let total_price = 0
-    for (let i = 0; i < order_foods.length; i++) {
-        total_price = total_price + order_foods[i].amount * order_foods[i].food.price
-    }
-    console.log(total_price)
-    const sample = new Order({
-        order_food: order_foods,
-        table_id: req.body.table_id,
-        total_price: total_price,
-    })
+    // If order with this table_id is already exist //todo
+    if (existedOrder) {
+
+        // define updated order foods list
+        let updateOrderedFood = existedOrder.order_food
+        
+        // define incoming order foods list
+        const incomingOrderedFood = order_foods
+
+        let newTotalPrice = existedOrder.total_price
+
+        // const new_order_foods = order_foods;
+
+        for (let i = 0; i < incomingOrderedFood.length; i++) {
+            updateOrderedFood.push(incomingOrderedFood[i])
+            newTotalPrice = newTotalPrice + incomingOrderedFood[i].amount * incomingOrderedFood[i].food.price   
+        }
+
+        const data = new Order({
+            order_food: updateOrderedFood,
+            table_id: table_id,
+            total_price: newTotalPrice,
+        })
+    
+    
+        data.save().then((result) => {
+            console.log(result)
+            res.status(201).json({ message: "Complete add data" })
+        }).catch((err) => {
+            res.status(501).json({ message: "Cannot add data" })
+        })
+
+    } 
+    // Else create new order
+    else {
+        let total_price = 0
+        for (let i = 0; i < order_foods.length; i++) {
+            total_price = total_price + order_foods[i].amount * order_foods[i].food.price
+        }
+
+        const data = new Order({
+            order_food: order_foods,
+            table_id: table_id,
+            total_price: total_price,
+        })
+
+        data.save().then((result) => {
+            console.log(result)
+            res.status(201).json({ message: "Complete add data" })
+        }).catch((err) => {
+            res.status(501).json({ message: "Cannot add data" })
+        })
 
 
-
-    sample.save().then((result) => {
-        console.log(result)
-        res.status(201).json({ message: "Complete add data" })
-    }).catch((err) => {
-        res.status(501).json({ message: "Cannot add data" })
-    })
+    }    
 }
 
 
