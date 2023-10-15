a626f7c1fef88fd6923b829525216114a204f922
Puttipongpn <fampoom1@gmail.com>
2023-10-15 15:01:42 +0700

    update get tables

--- frontend/src/app/pages/user/table/table.component.ts
+++ frontend/src/app/pages/user/table/table.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
-export class TableComponent {
- 
+export class TableComponent implements OnInit {
+
+  constructor(private router: Router) {}
+
+  table: any[] = [];
+  getTable() {
+    fetch("http://localhost:3000/table")
+      .then((response) => response.json())
+      .then((data) => {
+        this.table = data;
+        console.log(this.table);
+      })
+      .catch((error) => console.error(error));
+  }
+
+  ngOnInit(): void {
+      this.getTable();
+  }
+
 }
