805bfc06061b2c3e5df6bdcf7fe648c6da422026
B6327552 <Aceportgasonepiece@gmail.com>
2023-10-15 16:43:04 +0700

    status table

--- frontend/src/app/pages/admin/statustable/statustable.component.ts
+++ frontend/src/app/pages/admin/statustable/statustable.component.ts
@@ -3,16 +3,22 @@ import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-statustable',
   templateUrl: './statustable.component.html',
-  styleUrls: ['./statustable.component.css']
+  styleUrls: ['./statustable.component.css'],
 })
 export class StatustableComponent implements OnInit {
-
-
-  getTables(){
-    
+  api = 'http://localhost:3000/';
+  order: any = {};
+  getOrdered() {
+    fetch(`http://localhost:3000/order`)
+      .then((response) => response.json())
+      .then((data) => {
+        this.order = data; // Update the orderData object with the response data
+        console.log(this.order);
+      })
+      .catch((error) => console.error(error));
   }
+
   ngOnInit(): void {
-      
+    this.getOrdered();
   }
-  
 }
